696694583e83854d81343d65eebf65caf8fbcd54
Ahmed <ahmad21hussein@gmail.com>
2024-08-20 15:59:09 +0300

    stack

--- System/Data_Structures/Stack/stack.c
+++ System/Data_Structures/Stack/stack.c
@@ -1,2 +1,108 @@
 #include "../../Libraries/standard_libraries.h"
 #include "stack.h"
+
+void create_stack (stack *ps)
+{
+    if (ps)
+    {
+        ps->top=NULL;
+        ps->size=0;
+    }
+    else
+        printf ("Out of memory \n");
+}
+
+int is_stack_empty (stack *ps)
+{
+    if (ps)
+    {
+        return ps->top==NULL;
+    }
+    else
+        printf ("Out of memory \n");
+}
+
+int is_stack_full (stack *ps)
+{
+    if (ps)
+    {
+        return 0;
+    }
+    else
+        printf ("Out of memory \n");
+}
+
+void push (entry_type entry,stack *ps)
+{
+    if (ps)
+    {
+        node *pn=(node*) malloc(sizeof (node));
+        if (pn)
+        {
+            pn->entry=entry;
+            pn->next=ps->top;
+            ps->top=pn;
+            ps->size++;
+        }
+        else
+            printf ("Out of memory \n");
+    }
+    else
+        printf ("Out of memory \n");
+}
+
+void pop (entry_type *pe,stack *ps)
+{
+    if (ps)
+    {
+        *pe=ps->top->entry;
+        node *pn=ps->top;
+        ps->top=pn->next;
+        free (pn);
+        ps->size--;
+    }
+    else
+        printf ("Out of memory \n");
+}
+
+int stack_size (stack *ps)
+{
+    if (ps)
+    {
+        return ps->size;
+    }
+    else
+        printf ("Out of memory \n");
+}
+
+void clear_stack (stack *ps)
+{
+    if (ps)
+    {
+        node *pn=ps->top;
+        while (pn)
+        {
+            pn=pn->next;
+            free (ps->top);
+            ps->top=pn;
+        }
+        ps->size=0;
+    }
+    else
+        printf ("Out of memory \n");
+}
+
+void traverse_stack (stack *ps,void (*pf)(entry_type))
+{
+    if (ps)
+    {
+        node *pn=ps->top;
+        while (pn)
+        {
+            (*pf)(pn->entry);
+            pn=pn->next;
+        }
+    }
+    else
+        printf ("Out of memory \n");
+}
--- System/Data_Structures/Stack/stack.h
+++ System/Data_Structures/Stack/stack.h
@@ -3,4 +3,32 @@
 
 #include "../../Libraries/standard_libraries.h"
 
+typedef int entry_type;
+
+typedef struct node
+{
+
+    entry_type entry;
+    struct node *next;
+
+}node;
+
+typedef struct stack
+{
+
+    node *top;
+    int size;
+
+}stack;
+
+
+void create_stack (stack *ps);
+int is_stack_empty (stack *ps);
+int is_stack_full (stack *ps);
+void push (entry_type entry,stack *ps);
+void pop (entry_type *pe,stack *ps);
+void clear_stack (stack *ps);
+int stack_size (stack *ps);
+void traverse_stack (stack *ps,void (*pf)(entry_type));
+
 #endif // STACK_H_INCLUDED
