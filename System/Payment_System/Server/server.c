2e1f79161bd5a4f34b06a7bceef0c71dfb165f79
Ahmed <ahmad21hussein@gmail.com>
2024-09-02 19:11:02 +0300

    save a transaction in the list

--- System/Payment_System/Server/server.c
+++ System/Payment_System/Server/server.c
@@ -88,3 +88,49 @@ EN_serverError_t isAmountAvailable(ST_terminalData_t *termData,ST_accountsDB_t *
 
 }
 
+EN_serverError_t saveTransaction(ST_transaction_t *transData)
+{
+    if (transData == NULL)
+        return SAVING_FAILED;
+
+    int32_t count=Read_TransactionDB_FromFile ();
+
+    insert_Account (count,transData,&Transaction_DB);
+
+    if (Write_TransactionDB_ToFile () != 1)
+        return SAVING_FAILED;
+
+    return SERVER_OK;
+
+}
+
+
+void listSavedTransactions(void)
+{
+    node *pn = Transaction_DB.head;
+    char state[30];
+    while (pn)
+    {
+        ST_transaction_t *PtrAccount = (ST_transaction_t *)pn->ptr;
+
+        if      (PtrAccount->transState == APPROVED)                      strcpy (state,"APPROVED");
+        else if (PtrAccount->transState == DECLINED_INSUFFECIENT_FUND)    strcpy (state,"DECLINED_INSUFFECIENT_FUND");
+        else if (PtrAccount->transState == DECLINED_STOLEN_CARD)          strcpy (state,"DECLINED_STOLEN_CARD");
+        else if (PtrAccount->transState == FRAUD_CARD)                    strcpy (state,"FRAUD_CARD");
+        else if (PtrAccount->transState == INTERNAL_SERVER_ERROR)         strcpy (state,"INTERNAL_SERVER_ERROR");
+        else                                                              strcpy (state,"ERROR");
+
+        printf("###########################\n");
+        printf("Transaction Sequence Number: %d\n" ,PtrAccount->transactionSequenceNumber );
+        printf("Transaction Date: %s\n"            ,PtrAccount->terminalData.transactionDate);
+        printf("Transaction Amount: %.2f\n"        ,PtrAccount->terminalData.transAmount);
+        printf("Transaction State: %s\n"           ,state);
+        printf("Terminal Max Amount: %.2f\n"       ,PtrAccount->terminalData.maxTransAmount);
+        printf("Cardholder Name: %s\n"             ,PtrAccount->cardHolderData.cardHolderName);
+        printf("PAN: %s\n"                         ,PtrAccount->cardHolderData.primaryAccountNumber);
+        printf("Card Expiration Date: %s\n"        ,PtrAccount->cardHolderData.cardExpirationDate);
+        printf("###########################\n");
+
+        pn=pn->next;
+    }
+}
